96157fca098cfe1e7d5efc154d35918f0487034a
ervin-mitra <ervin.mitra@gopuff.com>
2021-02-03 16:41:44 -0800

    resolve schema error

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -37,31 +37,19 @@ const typeDefs = gql`
         SMALL
         LARGE
     }
-
-    """
-    Simple wrapper around our list of launches that contains a cursor to the
-    last item in the list. Pass this cursor to the launches query to fetch results
-    after these.
-    """
-    type LaunchConnection {
-        cursor: String!
-        hasMore: Boolean!
-        launches: [Launch]!
-    }
-
     type Query {
-        launches: (
-            """
-            The number of results to show. Must be >= 1. Default = 20
-            """
+        launches(
             pageSize: Int
-            """
-            If you add a cursor here, it will only return results _after_ this cursor
-            """
             after: String
         ) : LaunchConnection!
         launch(id: ID!): Launch
         me: User
+    }
+
+    type LaunchConnection { # add this below the Query type as an additional type.
+        cursor: String!
+        hasMore: Boolean!
+        launches: [Launch]!
     }
 
     type Mutation {
